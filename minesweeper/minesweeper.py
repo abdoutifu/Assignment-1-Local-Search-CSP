b19eac3da18a19072c160d6f82a4b8cdc45955ba
matthieupierrecouvreur <matthieu.pierre.couvreur@gmail.com>
2026-03-10 17:07:49 +0100

    Update minesweeper.py

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -3,88 +3,32 @@
 from pycsp3 import *
 
 
-def solve_minesweeper(clues: list[list[int]]) -> list[(int, int)]:
-    clear()
-
-# 1 : Problem formulation    
-    #variables : each cells
-    #domains : -1, 1, 2, 3, 4, 5, 6, 7, 8
-    #constraints : x_i, y_j = k>0, then 
-    #for a, b in [-1,0,1] with i=j=0 impossible
-    # x_(i+a), y(i+b)
-    #we have k cells = -1
+def solve_minesweeper(clues: list[list[int]]) -> list:
+    clear() #we clear prior variables and constraints
+    length, width = len(clues), len(clues[0])
+    assignements = VarArray(size=[length, width], dom={0, 1})
+    #an array of the dimensions of the map with the domain {0 : a mine, 1 : not a mine}
     
-    def enough_mines(cell, csp): #cell = [x,y]
-        mines = 0
-        for dx in [-1,0,1]:
-            for dy in [-1,0,1]:
-                if not (dx==0 and dy ==0):
-                    if csp[cell[1]+dy][cell[0]+dx]==-1:
-                        mines+=1
-        return mines == csp[cell[1]][cell[0]]
+    for i in range(length):
+        for j in range(width):
+            if clues[i][j] != -1:#if the cell is not already an identified mine
+                satisfy(assignements[i][j] == 0)
+                #the first constrain is to say that the cell is not a mine
+                neighboors = []
+                for dx in [-1,0,1]:
+                    for dy in [-1,0,1]:
+                        if (dx != 0 or dy != 0) and 0 <= i+dx < length and 0 <= j+dy < width:
+                                neighboors.append(assignements[i+dx][j+dy])
+                satisfy(Sum(neighboors) == clues[i][j])
+                #after gathering all the neighboors the 2nd constrain is to say
+                #that the sum of all values around are equal to the number written on 
+                #the middle cell if its not a mine
     
-    def neighboor_numbers(cell,csp):
-        nb = 0
-        for dx in [-1,0,1]:
-            for dy in [-1,0,1]:
-                if not (dx==0 and dy ==0):
-                    neighboor = csp[cell[1]+dy][cell[0]+dx]
-                    if type(neighboor)==int and neighboor>0:
-                        nb+= neighboor
-        return nb
-        
-    
-    #How do we define a cell with the most constraint ?
-    def select_unassigned_values(csp,assignement)-> [int,int]: 
-        #we return the best next cell to study
-        var = [-1,-1]
-        most_numbers = -1
-        
-        for y in range(len(csp)):
-            for x in range(len(csp[0])):
-
-                if csp[y][x] == " ":
-                    for dx in [-1,0,1]:
-                        for dy in [-1,0,1]:
-                            if not (dx==0 and dy ==0):
-                                if (x+dx)>0 and (x+dx)<len(csp[0]) and (y+dy)>0 and (y+dy)<len(csp):
-                                    if enough_mines([x+dx,y+dy], csp):
-                                        return [x,y]
-                    local_number = neighboor_numbers([x,y], csp)
-                    if most_numbers<  local_number:
-                        most_numbers = local_number
-                        var = [x,y]                                                     
-        return var
-    #think about the use of assignement
-    def order_domain_values(csp,var,assignement):
-        #We return the list of all possible values in a specific order
-        #We need to find how to organise these data
-        if enough_mines(var,  csp):
-            
-        return values
-    
-    #we try to get less possible values for a cell
-    def inference(csp,var,assignement):
-        return inferences
-
-    #Search algorithm
-    def backtrack(csp,assignement)-> list[(int, int)]:
-        if len(assignement)==len(clues[0])*len(clues):
-            return assignement
-        var = select_unassigned_values(csp,assignement)
-        for value in order_domain_values(csp,var, assignement):
-            if consistant(value,assignement):
-                assignement.append(value)
-                inferences = inference(csp,var,assignement)
-                if inferences != None:
-                    csp.append(inferences)
-                    result = backtrack(csp, assignement)
-                    if result != None:
-                        return result
-                    csp.remove(inferences)
-                assignement.remove(value)
-        return None
+    if solve() is SAT:
+        return [(x, y) for x in range(length) for y in range(width) if value(assignements[x][y]) == 1]
+    #if in the end map we have a mine, we save that value in the list as a tuple of its coordinates
     return None
+
 
 def check_solution(clues: list[list[int]], solution: list[(int, int)]) -> bool:
     n = len(clues)
